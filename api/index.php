980e82dff93bbc7c723163474171fb2f1437146e
Barita Davitya <bdavitya@gmail.com>
2024-10-19 16:54:37 +0700

    change index.php

--- api/index.php
+++ api/index.php
@@ -1,16 +1,22 @@
 <?php
-include "db.php";
-if(isset($_POST["submit"])){
-$name = $_POST["name"];
-$email = $_POST["email"];
-$insert = "INSERT INTO user (name,email) values('$name','$email')";
-if($db->exec($insert)){
-echo "inserted";
-}else{
-	echo "not inserted";
-}
+include "db.php"; // Meng-include file db.php yang mendefinisikan class database
+
+// Inisialisasi objek database
+$db = new database;
+
+if (isset($_POST["submit"])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $insert = "INSERT INTO user (name, email) VALUES ('$name', '$email')";
+    
+    if ($db->exec($insert)) {
+        echo "inserted";
+    } else {
+        echo "not inserted: " . $db->lastErrorMsg(); // Menampilkan pesan kesalahan
+    }
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 
@@ -20,15 +26,14 @@ echo "inserted";
 
 <body>
     <center>
-        <h3>simple Sqlite3 crud tutorial in php</h3>
-        <!--form-->
+        <h3>simple Sqlite3 CRUD tutorial in PHP</h3>
+        <!-- Form -->
         <form action="" method="post">
             <input type="text" name="name" placeholder="name"> <br>
             <input type="text" name="email" placeholder="email"> <br>
             <input type="submit" name="submit" value="save">
         </form>
-        <!--select data-->
-
+        <!-- Tampilkan data -->
         <table border="1">
             <tr>
                 <th>id(desc)</th>
@@ -38,25 +43,22 @@ echo "inserted";
                 <th>delete</th>
             </tr>
             <?php
-$select = "SELECT rowid,  * from user ORDER BY rowid DESC";
-$result = $db->query($select);
-while ($row = $result->fetchArray()) {
-	?>
+            // Pilih data setelah $db diinisialisasi
+            $select = "SELECT rowid, * FROM user ORDER BY rowid DESC";
+            $result = $db->query($select);
+            while ($row = $result->fetchArray()) {
+                ?>
             <tr>
                 <td><?php echo $row["rowid"]; ?></td>
                 <td><?php echo $row["name"]; ?></td>
                 <td><?php echo $row["email"]; ?></td>
-
-                <td><a href="edit.php?id=<?php echo $row['rowid'];?>">Edit</a></td>
-                <td><a href="delete.php?id=<?php echo $row['rowid'];?>">Delete</a></td>
+                <td><a href="edit.php?id=<?php echo $row['rowid']; ?>">Edit</a></td>
+                <td><a href="delete.php?id=<?php echo $row['rowid']; ?>">Delete</a></td>
             </tr>
             <?php
-  }
-
-
-?>
+            }
+            ?>
         </table>
-
     </center>
 </body>
 
