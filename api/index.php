39c354f423fa74ed5c606d260fdc82712e5c7b24
Barita Davitya <bdavitya@gmail.com>
2024-10-19 18:18:57 +0700

    update index.php

--- api/index.php
+++ api/index.php
@@ -11,6 +11,19 @@ if (isset($_POST['submit'])) {
         echo "Error inserting data.";
     }
 }
+
+$result = $db->query('SELECT * FROM users');
+if ($result) {
+    echo '<h2>Data Users</h2>';
+    echo '<table border="1">';
+    echo '<tr><th>ID</th><th>Name</th><th>Email</th></tr>';
+    while ($row = $result->fetchArray()) {
+        echo '<tr><td>' . $row['id'] . '</td><td>' . $row['name'] . '</td><td>' . $row['email'] . '</td></tr>';
+    }
+    echo '</table>';
+} else {
+    echo "Error fetching data: " . $db->lastErrorMsg();
+}
 ?>
 
 <!DOCTYPE html>
